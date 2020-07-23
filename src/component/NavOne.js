2ad0f67be79efce78549e11928d297528e95dc3f
Twel Tar <sulatttweltarbo16@gmail.com>
2020-07-24 01:08:41 +0630

    add Nav One

---
+++ src/component/NavOne.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './NavOne.css';
+
+const NavOne = () => {
+    return (
+        <header>
+            <a href="#" className="logo"><i className="fab fa-trello" style={{marginRight: 5 + 'px'}}></i>Trello</a>
+            <div className="nav-bar">
+                <nav>
+                    <a href="#" className="nav"><i className="fas fa-bars"></i></a>
+                    <a href="#" className="nav"><i className="fas fa-home"></i></a>
+                    <a href="#" className="nav boards"><i className="fab fa-trello"></i><p>Boards</p></a>
+                    <a href="#" className="search">
+                        <input type="search" className="searchbox" />
+                        <i id="search-icon" className="fas fa-search"></i>    
+                    </a>
+                </nav>
+            </div>
+            <div className="option-bar">
+                <a href="#" className="nav"><i className="fas fa-plus"></i></a>
+                <a href="#" className="nav"><i className="fas fa-info-circle"></i></a>
+                <a href="#" className="nav"><i className="far fa-bell"></i></a>
+                <img src="../src/assets/ttb.jpg" alt="img" id="profile" />
+            </div>
+        </header>
+    )
+}
+
+export default NavOne;
